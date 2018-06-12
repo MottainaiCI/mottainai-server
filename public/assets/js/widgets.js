b714577d27104db999561d547fe6df7cf0ee6849
Ettore Di Giacinto <mudler@gentoo.org>
2018-06-12 15:35:47 +0200

    Add charts to landing page

--- public/assets/js/widgets.js
+++ public/assets/js/widgets.js
@@ -17,248 +17,299 @@
 
 
 
+    $.ajax({
+        url: "/api/stats",
+      })
+      .done(function( data ) {
+           var dailykeys = [];
+           var dailyval = [];
+           for(var k in data.created_daily) dailykeys.push(k) && dailyval.push(data.created_daily[k]);
+
+           var failedkeys = [];
+           var failedval = [];
+           for(var k in data.failed_daily) failedkeys.push(k) && failedval.push(data.failed_daily[k]);
+
+           var erroredkeys = [];
+           var erroredval = [];
+           for(var k in data.errored_daily) erroredkeys.push(k) && erroredval.push(data.errored_daily[k]);
+
+           var succeededkeys = [];
+           var succeededval = [];
+           for(var k in data.succeeded_daily) succeededkeys.push(k) && succeededval.push(data.succeeded_daily[k]);
 
 
-    //WidgetChart 1
-    var ctx = document.getElementById( "widgetChart1" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [65, 59, 84, 84, 51, 55, 40],
-                label: 'Dataset',
-                backgroundColor: 'transparent',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
+           //WidgetChart 1
+           var ctx = document.getElementById( "dailychart" );
+           ctx.height = 150;
+           var myChart = new Chart( ctx, {
+               type: 'line',
+               data: {
+                   labels: dailykeys,
+                   type: 'line',
+                   datasets: [ {
+                       data: dailyval,
+                       label: 'Created tasks',
+                       backgroundColor: 'transparent',
+                       borderColor: 'rgba(255,255,255,.55)',
+                   }, ]
+               },
+               options: {
 
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            tooltips: {
-                mode: 'index',
-                titleFontSize: 12,
-                titleFontColor: '#000',
-                bodyFontColor: '#000',
-                backgroundColor: '#fff',
-                titleFontFamily: 'Montserrat',
-                bodyFontFamily: 'Montserrat',
-                cornerRadius: 3,
-                intersect: false,
-            },
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
-
-    //WidgetChart 2
-    var ctx = document.getElementById( "widgetChart2" );
-    ctx.height = 150;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [1, 18, 9, 17, 34, 22, 11],
-                label: 'Dataset',
-                backgroundColor: '#63c2de',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
-
-            maintainAspectRatio: false,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            tooltips: {
-                mode: 'index',
-                titleFontSize: 12,
-                titleFontColor: '#000',
-                bodyFontColor: '#000',
-                backgroundColor: '#fff',
-                titleFontFamily: 'Montserrat',
-                bodyFontFamily: 'Montserrat',
-                cornerRadius: 3,
-                intersect: false,
-            },
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    tension: 0.00001,
-                    borderWidth: 1
-                },
-                point: {
-                    radius: 4,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
+                   maintainAspectRatio: false,
+                   legend: {
+                       display: false
+                   },
+                   responsive: true,
+                   tooltips: {
+                       mode: 'index',
+                       titleFontSize: 12,
+                       titleFontColor: '#000',
+                       bodyFontColor: '#000',
+                       backgroundColor: '#fff',
+                       titleFontFamily: 'Montserrat',
+                       bodyFontFamily: 'Montserrat',
+                       cornerRadius: 3,
+                       intersect: false,
+                   },
+                   scales: {
+                       xAxes: [ {
+                           gridLines: {
+                               color: 'transparent',
+                               zeroLineColor: 'transparent'
+                           },
+                           ticks: {
+                               fontSize: 2,
+                               fontColor: 'transparent'
+                           }
+                       } ],
+                       yAxes: [ {
+                           display:false,
+                           ticks: {
+                               display: false,
+                           }
+                       } ]
+                   },
+                   title: {
+                       display: false,
+                   },
+                   elements: {
+                       line: {
+                           borderWidth: 1
+                       },
+                       point: {
+                           radius: 4,
+                           hitRadius: 10,
+                           hoverRadius: 4
+                       }
+                   }
+               }
+           } );
 
 
 
-    //WidgetChart 3
-    var ctx = document.getElementById( "widgetChart3" );
-    ctx.height = 70;
-    var myChart = new Chart( ctx, {
-        type: 'line',
-        data: {
-            labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-            type: 'line',
-            datasets: [ {
-                data: [78, 81, 80, 45, 34, 12, 40],
-                label: 'Dataset',
-                backgroundColor: 'rgba(255,255,255,.2)',
-                borderColor: 'rgba(255,255,255,.55)',
-            }, ]
-        },
-        options: {
+      var ctx2 = document.getElementById( "erroredchart" );
+      ctx2.height = 150;
+      var myChart2 = new Chart( ctx2, {
+          type: 'line',
+          data: {
+              labels: erroredkeys,
+              type: 'line',
+              datasets: [ {
+                  data: erroredval,
+                  label: 'Errored tasks',
+                  backgroundColor: 'transparent',
+                  borderColor: 'rgba(255,255,255,.55)',
+              }, ]
+          },
+          options: {
 
-            maintainAspectRatio: true,
-            legend: {
-                display: false
-            },
-            responsive: true,
-            // tooltips: {
-            //     mode: 'index',
-            //     titleFontSize: 12,
-            //     titleFontColor: '#000',
-            //     bodyFontColor: '#000',
-            //     backgroundColor: '#fff',
-            //     titleFontFamily: 'Montserrat',
-            //     bodyFontFamily: 'Montserrat',
-            //     cornerRadius: 3,
-            //     intersect: false,
-            // },
-            scales: {
-                xAxes: [ {
-                    gridLines: {
-                        color: 'transparent',
-                        zeroLineColor: 'transparent'
-                    },
-                    ticks: {
-                        fontSize: 2,
-                        fontColor: 'transparent'
-                    }
-                } ],
-                yAxes: [ {
-                    display:false,
-                    ticks: {
-                        display: false,
-                    }
-                } ]
-            },
-            title: {
-                display: false,
-            },
-            elements: {
-                line: {
-                    borderWidth: 2
-                },
-                point: {
-                    radius: 0,
-                    hitRadius: 10,
-                    hoverRadius: 4
-                }
-            }
-        }
-    } );
-
-
-    //WidgetChart 4
-    var ctx = document.getElementById( "widgetChart4" );
-    ctx.height = 70;
-    var myChart = new Chart( ctx, {
-        type: 'bar',
-        data: {
-            labels: ['', '', '', '', '', '', '', '', '', '', '', '', '', '', '', ''],
-            datasets: [
-                {
-                    label: "My First dataset",
-                    data: [78, 81, 80, 45, 34, 12, 40, 75, 34, 89, 32, 68, 54, 72, 18, 98],
-                    borderColor: "rgba(0, 123, 255, 0.9)",
-                    //borderWidth: "0",
-                    backgroundColor: "rgba(255,255,255,.3)"
-                }
-            ]
-        },
-        options: {
-              maintainAspectRatio: true,
+              maintainAspectRatio: false,
               legend: {
-                display: false
-            },
-            scales: {
-                xAxes: [{
+                  display: false
+              },
+              responsive: true,
+              tooltips: {
+                  mode: 'index',
+                  titleFontSize: 12,
+                  titleFontColor: '#000',
+                  bodyFontColor: '#000',
+                  backgroundColor: '#fff',
+                  titleFontFamily: 'Montserrat',
+                  bodyFontFamily: 'Montserrat',
+                  cornerRadius: 3,
+                  intersect: false,
+              },
+              scales: {
+                  xAxes: [ {
+                      gridLines: {
+                          color: 'transparent',
+                          zeroLineColor: 'transparent'
+                      },
+                      ticks: {
+                          fontSize: 2,
+                          fontColor: 'transparent'
+                      }
+                  } ],
+                  yAxes: [ {
+                      display:false,
+                      ticks: {
+                          display: false,
+                      }
+                  } ]
+              },
+              title: {
                   display: false,
-                  categoryPercentage: 1,
-                  barPercentage: 0.5
-                }],
-                yAxes: [ {
-                    display: false
-                } ]
-            }
-        }
-    } );
+              },
+              elements: {
+                  line: {
+                      borderWidth: 1
+                  },
+                  point: {
+                      radius: 4,
+                      hitRadius: 10,
+                      hoverRadius: 4
+                  }
+              }
+          }
+      } );
+
+
+ var ctx3 = document.getElementById( "failedchart" );
+ ctx3.height = 150;
+ var myChart3 = new Chart( ctx3, {
+     type: 'line',
+     data: {
+         labels: failedkeys,
+         type: 'line',
+         datasets: [ {
+             data: failedval,
+             label: 'Failed tasks',
+             backgroundColor: 'transparent',
+             borderColor: 'rgba(255,255,255,.55)',
+         }, ]
+     },
+     options: {
+
+         maintainAspectRatio: false,
+         legend: {
+             display: false
+         },
+         responsive: true,
+         tooltips: {
+             mode: 'index',
+             titleFontSize: 12,
+             titleFontColor: '#000',
+             bodyFontColor: '#000',
+             backgroundColor: '#fff',
+             titleFontFamily: 'Montserrat',
+             bodyFontFamily: 'Montserrat',
+             cornerRadius: 3,
+             intersect: false,
+         },
+         scales: {
+             xAxes: [ {
+                 gridLines: {
+                     color: 'transparent',
+                     zeroLineColor: 'transparent'
+                 },
+                 ticks: {
+                     fontSize: 2,
+                     fontColor: 'transparent'
+                 }
+             } ],
+             yAxes: [ {
+                 display:false,
+                 ticks: {
+                     display: false,
+                 }
+             } ]
+         },
+         title: {
+             display: false,
+         },
+         elements: {
+             line: {
+                 borderWidth: 1
+             },
+             point: {
+                 radius: 4,
+                 hitRadius: 10,
+                 hoverRadius: 4
+             }
+         }
+     }
+ } );
+
+
+
+  var ctx4 = document.getElementById( "succeededchart" );
+  ctx4.height = 150;
+  var myChart4 = new Chart( ctx4, {
+      type: 'line',
+      data: {
+          labels: succeededkeys,
+          type: 'line',
+          datasets: [ {
+              data: succeededval,
+              label: 'Succeeded tasks',
+              backgroundColor: 'transparent',
+              borderColor: 'rgba(255,255,255,.55)',
+          }, ]
+      },
+      options: {
+
+          maintainAspectRatio: false,
+          legend: {
+              display: false
+          },
+          responsive: true,
+          tooltips: {
+              mode: 'index',
+              titleFontSize: 12,
+              titleFontColor: '#000',
+              bodyFontColor: '#000',
+              backgroundColor: '#fff',
+              titleFontFamily: 'Montserrat',
+              bodyFontFamily: 'Montserrat',
+              cornerRadius: 3,
+              intersect: false,
+          },
+          scales: {
+              xAxes: [ {
+                  gridLines: {
+                      color: 'transparent',
+                      zeroLineColor: 'transparent'
+                  },
+                  ticks: {
+                      fontSize: 2,
+                      fontColor: 'transparent'
+                  }
+              } ],
+              yAxes: [ {
+                  display:false,
+                  ticks: {
+                      display: false,
+                  }
+              } ]
+          },
+          title: {
+              display: false,
+          },
+          elements: {
+              line: {
+                  borderWidth: 1
+              },
+              point: {
+                  radius: 4,
+                  hitRadius: 10,
+                  hoverRadius: 4
+              }
+          }
+      }
+  } );
+
+});
+
 
 
 
