2f4f6e57f8f9df4a2cbf217c05ba80da747dc081
Ettore Di Giacinto <mudler@gentoo.org>
2018-10-19 19:31:39 +0200

    vagrant: use shared buildpath for home folders

--- pkg/tasks/vagrant.go
+++ pkg/tasks/vagrant.go
@@ -222,10 +222,10 @@ func (d *VagrantExecutor) Setup(docID string) error {
 
 	if d.IsVirtualBox() {
 		// Confine VirtualBox environment to BuildDir
-		os.Setenv("VAGRANT_HOME", d.Context.BuildDir)
-		os.Setenv("VBOX_USER_HOME", d.Context.BuildDir)
+		os.Setenv("VAGRANT_HOME", d.Config.GetAgent().BuildPath)
+		os.Setenv("VBOX_USER_HOME", d.Config.GetAgent().BuildPath)
 		os.Unsetenv("HOME")
-		os.Setenv("HOME", d.Context.BuildDir)
+		os.Setenv("HOME", d.Config.GetAgent().BuildPath)
 		// XXX: Fix hanging shutdown
 		os.Setenv("SHUTDOWN", "poweroff")
 	}
