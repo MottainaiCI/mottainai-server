818aa16a256a780700f5ed56c236a3c7b68dd7e3
Adib Saad <adib.saad@gmail.com>
2020-12-04 21:55:58 -0500

    Input validation

--- routes/api/client/auth/auth.go
+++ routes/api/client/auth/auth.go
@@ -24,23 +24,171 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package auth
 
 import (
+	"fmt"
 	"github.com/MottainaiCI/mottainai-server/pkg/context"
 	database "github.com/MottainaiCI/mottainai-server/pkg/db"
 	setting "github.com/MottainaiCI/mottainai-server/pkg/settings"
 	user "github.com/MottainaiCI/mottainai-server/pkg/user"
+	"github.com/Unknwon/com"
 	"github.com/go-macaron/binding"
 	"github.com/go-macaron/captcha"
 	log "gopkg.in/clog.v1"
 	"gopkg.in/macaron.v1"
+	"reflect"
+	"regexp"
+	"strings"
 
 	v1 "github.com/MottainaiCI/mottainai-server/routes/schema/v1"
 )
+
+type Form interface {
+	binding.Validator
+}
+
+// Assign assign form values back to the template data.
+func Assign(form interface{}, data map[string]interface{}) {
+	typ := reflect.TypeOf(form)
+	val := reflect.ValueOf(form)
+
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+		val = val.Elem()
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+
+		fieldName := field.Tag.Get("form")
+		// Allow ignored fields in the struct
+		if fieldName == "-" {
+			continue
+		} else if len(fieldName) == 0 {
+			fieldName = com.ToSnakeCase(field.Name)
+		}
+
+		data[fieldName] = val.Field(i).Interface()
+	}
+}
+func getRuleBody(field reflect.StructField, prefix string) string {
+	for _, rule := range strings.Split(field.Tag.Get("binding"), ";") {
+		if strings.HasPrefix(rule, prefix) {
+			return rule[len(prefix) : len(rule)-1]
+		}
+	}
+	return ""
+}
+
+const ERR_ALPHA_DASH_DOT_SLASH = "AlphaDashDotSlashError"
+
+var AlphaDashDotSlashPattern = regexp.MustCompile("[^\\d\\w-_\\./]")
+
+func init() {
+	binding.SetNameMapper(com.ToSnakeCase)
+	binding.AddRule(&binding.Rule{
+		IsMatch: func(rule string) bool {
+			return rule == "AlphaDashDotSlash"
+		},
+		IsValid: func(errs binding.Errors, name string, v interface{}) (bool, binding.Errors) {
+			if AlphaDashDotSlashPattern.MatchString(fmt.Sprintf("%v", v)) {
+				errs.Add([]string{name}, ERR_ALPHA_DASH_DOT_SLASH, "AlphaDashDotSlash")
+				return false, errs
+			}
+			return true, errs
+		},
+	})
+}
+
+func getSize(field reflect.StructField) string {
+	return getRuleBody(field, "Size(")
+}
+
+func getMinSize(field reflect.StructField) string {
+	return getRuleBody(field, "MinSize(")
+}
+
+func getMaxSize(field reflect.StructField) string {
+	return getRuleBody(field, "MaxSize(")
+}
+
+func getInclude(field reflect.StructField) string {
+	return getRuleBody(field, "Include(")
+}
+
+// todo better validation handling and messages
+func validate(errs binding.Errors, data map[string]interface{}, f Form, l macaron.Locale) binding.Errors {
+	if errs.Len() == 0 {
+		return errs
+	}
+
+	data["HasError"] = true
+	Assign(f, data)
+
+	typ := reflect.TypeOf(f)
+	val := reflect.ValueOf(f)
+
+	if typ.Kind() == reflect.Ptr {
+		typ = typ.Elem()
+		val = val.Elem()
+	}
+
+	for i := 0; i < typ.NumField(); i++ {
+		field := typ.Field(i)
+
+		fieldName := field.Tag.Get("form")
+		// Allow ignored fields in the struct
+		if fieldName == "-" {
+			continue
+		}
+
+		if errs[0].FieldNames[0] == field.Name {
+			data["Err_"+field.Name] = true
+
+			trName := field.Tag.Get("locale")
+			if len(trName) == 0 {
+				trName = "form." + field.Name
+			} else {
+				trName = trName
+			}
+
+			switch errs[0].Classification {
+			case binding.ERR_REQUIRED:
+				data["ErrorMsg"] = trName + "form.require_error"
+			case binding.ERR_ALPHA_DASH:
+				data["ErrorMsg"] = trName + "form.alpha_dash_error"
+			case binding.ERR_ALPHA_DASH_DOT:
+				data["ErrorMsg"] = trName + "form.alpha_dash_dot_error"
+			case ERR_ALPHA_DASH_DOT_SLASH:
+				data["ErrorMsg"] = trName + "form.alpha_dash_dot_slash_error"
+			case binding.ERR_SIZE:
+				data["ErrorMsg"] = trName + "form.size_error" + getSize(field)
+			case binding.ERR_MIN_SIZE:
+				data["ErrorMsg"] = trName + "form.min_size_error" + getMinSize(field)
+			case binding.ERR_MAX_SIZE:
+				data["ErrorMsg"] = trName + "form.max_size_error" + getMaxSize(field)
+			case binding.ERR_EMAIL:
+				data["ErrorMsg"] = trName + "form.email_error"
+			case binding.ERR_URL:
+				data["ErrorMsg"] = trName + "form.url_error"
+			case binding.ERR_INCLUDE:
+				data["ErrorMsg"] = trName + "form.include_error" + getInclude(field)
+			default:
+				data["ErrorMsg"] = "form.unknown_error" + " " + errs[0].Classification
+			}
+			return errs
+		}
+	}
+	return errs
+}
 
 type SignIn struct {
 	UserName    string `binding:"Required;MaxSize(254)"`
 	Password    string `binding:"Required;MaxSize(255)"`
 	LoginSource int64
 	Remember    bool
+}
+
+func (f *SignIn) Validate(ctx *macaron.Context, errs binding.Errors) binding.Errors {
+	return validate(errs, ctx.Data, f, ctx.Locale)
 }
 
 type SignUp struct {
@@ -50,6 +198,10 @@ type SignUp struct {
 	PasswordConfirm string
 	Captcha         string `binding:"Required;MaxSize(10)"`
 	CaptchaId       string `binding:"Required;MaxSize(30)" json:"captcha_id"`
+}
+
+func (f *SignUp) Validate(ctx *macaron.Context, errs binding.Errors) binding.Errors {
+	return validate(errs, ctx.Data, f, ctx.Locale)
 }
 
 type UserResp struct {
@@ -64,10 +216,6 @@ type ErrorResp struct {
 	Error string `json:"error"`
 }
 
-//func (f *SignIn) Validate(ctx *macaron.Context, errs binding.Errors) binding.Errors {
-// return validate(errs, ctx.Data, f, ctx.Locale)
-//}
-
 func Register(c *context.Context, cpt *captcha.Captcha, f SignUp, db *database.Database) {
 	uuu, err := db.Driver.GetSettingByKey(setting.SYSTEM_SIGNUP_ENABLED)
 	if err == nil {
@@ -75,6 +223,11 @@ func Register(c *context.Context, cpt *captcha.Captcha, f SignUp, db *database.D
 			c.JSON(503, map[string]string{"error": "Signup disabled"})
 			return
 		}
+	}
+
+	if c.HasError() {
+		c.JSON(400, map[string]string{"error": c.Flash.ErrorMsg})
+		return
 	}
 
 	if !cpt.Verify(f.CaptchaId, f.Captcha) {
@@ -114,6 +267,11 @@ func Register(c *context.Context, cpt *captcha.Captcha, f SignUp, db *database.D
 func Login(c *context.Context, f SignIn, db *database.Database) {
 	var err error
 	var u user.User
+
+	if c.HasError() {
+		c.JSON(400, map[string]string{"error": c.Flash.ErrorMsg})
+		return
+	}
 
 	onlyuser_val, err := db.Driver.GetSettingByKey(
 		setting.SYSTEM_SIGNIN_ONLY_USERVALIDATION)
