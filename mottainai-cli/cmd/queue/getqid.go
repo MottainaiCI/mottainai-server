565d5098f73c8e5547b591cecb1cd6dc794fb172
Daniele Rondina <geaaru@sabayonlinux.org>
2021-05-17 22:39:41 +0200

    mottainai-cli: Add new commands and drop task execute

--- mottainai-cli/cmd/queue/getqid.go
+++ mottainai-cli/cmd/queue/getqid.go
@@ -17,78 +17,60 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 */
 
-package nodequeue
+package queue
 
 import (
-	"bytes"
-	"encoding/json"
 	"fmt"
 	"log"
+	"os"
 
 	schema "github.com/MottainaiCI/mottainai-server/routes/schema"
-	v1 "github.com/MottainaiCI/mottainai-server/routes/schema/v1"
 
-	tools "github.com/MottainaiCI/mottainai-server/mottainai-cli/common"
 	client "github.com/MottainaiCI/mottainai-server/pkg/client"
-	queues "github.com/MottainaiCI/mottainai-server/pkg/queues"
 	setting "github.com/MottainaiCI/mottainai-server/pkg/settings"
+	v1 "github.com/MottainaiCI/mottainai-server/routes/schema/v1"
 	cobra "github.com/spf13/cobra"
 	viper "github.com/spf13/viper"
 )
 
-func newNodeQueueShowCommand(config *setting.Config) *cobra.Command {
+func newQueueGetQidCommand(config *setting.Config) *cobra.Command {
 	var cmd = &cobra.Command{
-		Use:   "show <nodequeue-id> [OPTIONS]",
-		Short: "Show a node queue data",
-		Args:  cobra.RangeArgs(1, 1),
+		Use:   "get-qid <queue-name> [OPTIONS]",
+		Short: "Retrieve Queue Id by queue name (dev only)",
+		Args:  cobra.OnlyValidArgs,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			if len(args) == 0 {
+				fmt.Println("Missing queue name")
+				os.Exit(1)
+			}
+		},
 		Run: func(cmd *cobra.Command, args []string) {
-			var n queues.NodeQueues
 			var v *viper.Viper = config.Viper
+			var qid string
 
 			fetcher := client.NewTokenClient(
 				v.GetString("master"), v.GetString("apikey"), config,
 			)
 
-			id := args[0]
-			if len(id) == 0 {
-				log.Fatalln("You need to define a node queue id")
-			}
-
 			req := &schema.Request{
-				Route: v1.Schema.GetNodeQueueRoute("show_byagent"),
+				Route: v1.Schema.GetQueueRoute("get_qid"),
 				Options: map[string]interface{}{
-					":nodeid": id,
+					":name": args[0],
 				},
-				Target: &n,
+				Target: &qid,
 			}
-			msg := map[string]interface{}{
-				"akey":   "j1ZvSpC1405KjYfkCB6Uv1AKj2idwO",
-				"nodeid": id,
-			}
-			b, _ := json.Marshal(msg)
-
-			req.Body = bytes.NewBuffer(b)
-
 			err := fetcher.Handle(req)
-			fmt.Println("RES ", string(req.ResponseRaw))
-			tools.CheckError(err)
-			/*
-				req := &schema.Request{
-					Route: v1.Schema.GetNodeQueueRoute("show"),
-					Options: map[string]interface{}{
-						":id": id,
-					},
-					Target: &n,
-				}
-				err := fetcher.Handle(req)
-				tools.CheckError(err)
-
-				b, err := json.MarshalIndent(n, "", "  ")
-				if err != nil {
+			if err != nil {
+				if req.Response != nil {
+					fmt.Println("ERROR: ", req.Response.StatusCode)
+					fmt.Println(string(req.ResponseRaw))
+				} else {
 					log.Fatalln("error:", err)
 				}
-				fmt.Println(string(b))
-			*/
+				os.Exit(1)
+			}
+
+			fmt.Println(qid)
 		},
 	}
 
