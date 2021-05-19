4435e1aa869c05a4dd98149a83e09480c6df67eb
Daniele Rondina <geaaru@sabayonlinux.org>
2021-05-19 18:40:30 +0200

    mcli/cmd/node/list: Add --json option

--- mottainai-cli/cmd/node/list.go
+++ mottainai-cli/cmd/node/list.go
@@ -21,6 +21,8 @@ along with this program. If not, see <http://www.gnu.org/licenses/>.
 package node
 
 import (
+	"encoding/json"
+	"fmt"
 	"log"
 	"os"
 
@@ -45,6 +47,7 @@ func newNodeListCommand(config *setting.Config) *cobra.Command {
 			var node_table [][]string
 			var v *viper.Viper = config.Viper
 
+			jsonOutput, _ := cmd.Flags().GetBool("json")
 			fetcher := client.NewTokenClient(v.GetString("master"), v.GetString("apikey"), config)
 
 			req := &schema.Request{
@@ -57,21 +60,29 @@ func newNodeListCommand(config *setting.Config) *cobra.Command {
 				log.Fatalln("error:", err)
 			}
 
-			for _, i := range n {
-				node_table = append(node_table, []string{i.ID, i.Hostname, i.User, i.Pass, i.Key, i.NodeID})
-			}
+			if jsonOutput {
+				data, _ := json.Marshal(n)
+				fmt.Println(string(data))
+			} else {
+				for _, i := range n {
+					node_table = append(node_table, []string{i.ID, i.Hostname, i.User, i.Pass, i.Key, i.NodeID})
+				}
 
-			table := tablewriter.NewWriter(os.Stdout)
-			table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
-			table.SetCenterSeparator("|")
-			table.SetHeader([]string{"ID", "Hostname", "User", "Pass", "Key", "UUID"})
+				table := tablewriter.NewWriter(os.Stdout)
+				table.SetBorders(tablewriter.Border{Left: true, Top: false, Right: true, Bottom: false})
+				table.SetCenterSeparator("|")
+				table.SetHeader([]string{"ID", "Hostname", "User", "Pass", "Key", "UUID"})
 
-			for _, v := range node_table {
-				table.Append(v)
+				for _, v := range node_table {
+					table.Append(v)
+				}
+				table.Render()
 			}
-			table.Render()
 		},
 	}
+
+	var flags = cmd.Flags()
+	flags.Bool("json", false, "JSON output")
 
 	return cmd
 }
