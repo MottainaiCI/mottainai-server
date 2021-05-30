cd01d07ef72f7fbee0998d5fcd2b12a60503a5db
Daniele Rondina <geaaru@sabayonlinux.org>
2021-05-30 21:42:57 +0200

    Add entities pkg

---
+++ pkg/entities/entities.go
@@ -0,0 +1,99 @@
+/*
+
+Copyright (C) 2021  Daniele Rondina <geaaru@sabayonlinux.org>
+
+Credits goes also to Gogs authors, some code portions and re-implemented design
+are also coming from the Gogs project, which is using the go-macaron framework
+and was really source of ispiration. Kudos to them!
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+*/
+package entities
+
+type MottainaiEntity string
+
+const (
+	Webhooks      MottainaiEntity = "webhooks"
+	Tasks         MottainaiEntity = "tasks"
+	Secrets       MottainaiEntity = "secrets"
+	Users         MottainaiEntity = "users"
+	Plans         MottainaiEntity = "plans"
+	Pipelines     MottainaiEntity = "pipelines"
+	Nodes         MottainaiEntity = "nodes"
+	Namespaces    MottainaiEntity = "namespaces"
+	Tokens        MottainaiEntity = "tokens"
+	Artefacts     MottainaiEntity = "artefacts"
+	Storages      MottainaiEntity = "storage"
+	Organizations MottainaiEntity = "organizations"
+	Settings      MottainaiEntity = "settings"
+	Queues        MottainaiEntity = "queues"
+	NodeQueues    MottainaiEntity = "nodequeues"
+)
+
+func GetMottainaiEntities() []MottainaiEntity {
+	return []MottainaiEntity{
+		Webhooks,
+		Tasks,
+		Secrets,
+		Users,
+		Plans,
+		Pipelines,
+		Nodes,
+		Namespaces,
+		Tokens,
+		Artefacts,
+		Storages,
+		Organizations,
+		Settings,
+		Queues,
+		NodeQueues,
+	}
+}
+
+func (e *MottainaiEntity) String() string {
+	switch *e {
+	case Webhooks:
+		return "webhooks"
+	case Tasks:
+		return "tasks"
+	case Secrets:
+		return "secrets"
+	case Users:
+		return "users"
+	case Plans:
+		return "plans"
+	case Pipelines:
+		return "pipelines"
+	case Nodes:
+		return "nodes"
+	case Namespaces:
+		return "namespaces"
+	case Tokens:
+		return "tokens"
+	case Artefacts:
+		return "artefacts"
+	case Storages:
+		return "storage"
+	case Organizations:
+		return "organizations"
+	case Settings:
+		return "settings"
+	case Queues:
+		return "queues"
+	case NodeQueues:
+		return "nodequeues"
+	}
+	return ""
+}
