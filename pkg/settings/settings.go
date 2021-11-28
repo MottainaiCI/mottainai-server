f9d1202769cd37e7f34629e637a18414357597e3
Daniele Rondina <geaaru@sabayonlinux.org>
2021-11-28 18:10:43 +0100

    Update version @v0.1.1

--- pkg/settings/settings.go
+++ pkg/settings/settings.go
@@ -31,7 +31,7 @@ import (
 )
 
 const (
-	MOTTAINAI_VERSION    = "0.1.0"
+	MOTTAINAI_VERSION    = "0.1.1"
 	MOTTAINAI_ENV_PREFIX = "MOTTAINAI"
 	MOTTAINAI_CONFIGNAME = "mottainai-server"
 	MOTTAINAI_CONFIGPATH = "/etc/mottainai"
